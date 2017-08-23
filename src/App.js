ab40cf08c6d8574cf0549c2fc5c818b563727ebc
Albert Sheu <albert.sheu@gmail.com>
2017-08-23 15:25:09 -0700

    Add mock background

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 
 import CardEditable from './components/CardEditable';
 
+const BACKGROUND_URL = '/sam-ferrara-318709.jpg';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{
+        background: `url(${BACKGROUND_URL})`,
+        backgroundSize: 'cover'
+      }}>
         <CardEditable />
       </div>
     );
