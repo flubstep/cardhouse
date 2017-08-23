c3793fd94f477679228d0069badd663e2bc5c66c
Albert Sheu <albert.sheu@gmail.com>
2017-08-23 15:12:07 -0700

    Move mock data to its own module and add draggable edges

--- src/components/CardEditable.js
+++ src/components/CardEditable.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 
-const MOCK_IMAGE_URL = 'https://images.unsplash.com/photo-1461272395971-7c6ffaa435b7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=720&h=720&fit=crop&s=8b41b3430d730690567af6feb472f40a';
-const MOCK_TITLE = 'Martial Combat';
-const MOCK_TEXT = 'Sword and shield, pole-arms, axes, war hammers, etc. If you can kill something by swinging it you can learn how to do it at a hero academy. Every common style of combat is covered in this course guaranteeing that a student will have at the very least a familiarity with most common martial weapons.';
+const { IMAGE_URL, TITLE, TEXT } = require('./mockData.json');
 
 export default class CardEditable extends Component {
 
@@ -11,32 +9,142 @@ export default class CardEditable extends Component {
     this.state = {
       height: 480,
       width: 320,
-      imageUrl: MOCK_IMAGE_URL,
-      title: MOCK_TITLE,
-      text: MOCK_TEXT
+      imageUrl: IMAGE_URL,
+      title: TITLE,
+      text: TEXT,
+      dragStart: null
     };
+  }
+
+  componentDidMount() {
+    window.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('mouseup', this.handleMouseUp);
+  }
+
+  handleMouseDown = (e, el) => {
+    const dragStart = {
+      el,
+      x0: e.clientX,
+      y0: e.clientY,
+      originalHeight: this.state.height,
+      originalWidth: this.state.width
+    };
+    this.setState({ dragStart });
+  }
+
+  handleMouseMove = (e) => {
+    if (this.state.dragStart) {
+      const { el, x0, y0, originalWidth, originalHeight } = this.state.dragStart;
+      const dx = e.clientX - x0;
+      const dy = e.clientY - y0;
+      switch (el) {
+        case 'top':
+          this.setState({ height: originalHeight - 2 * dy });
+          break;
+        case 'bottom':
+          this.setState({ height: originalHeight + 2 * dy });
+          break;
+        case 'left':
+          this.setState({ width: originalWidth - 2 * dx });
+          break;
+        case 'right':
+          this.setState({ width: originalWidth + 2 * dx });
+          break;
+        case 'bottomleft':
+          this.setState({ height: originalHeight + 2 * dy, width: originalWidth - 2 * dx });
+          break;
+        case 'bottomright':
+          this.setState({ height: originalHeight + 2 * dy, width: originalWidth + 2 * dx });
+          break;
+        case 'topleft':
+          this.setState({ height: originalHeight - 2 * dy, width: originalWidth - 2 * dx });
+          break;
+        case 'topright':
+          this.setState({ height: originalHeight - 2 * dy, width: originalWidth + 2 * dx });
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  handleMouseUp = (e) => {
+    if (this.state.dragStart) {
+      this.setState({
+        dragStart: null
+      });
+    }
   }
 
   render() {
     const { width, height } = this.state;
     return (
-      <div className='CardEditable flex-column' style={{ width, height }}>
+      <div className="CardEditable flex-column" style={{ width, height }}>
         { this.state.imageUrl && (
-          <div className='img-container' style={{ width, height: height / 2 }}>
+          <div className="img-container" style={{ width, height: height / 2 }}>
             <img alt={this.state.title} src={this.state.imageUrl} width={width} />
           </div>
         ) }
-        <div className='text-container'>
+        <div className="text-container">
           <h1>{ this.state.title }</h1>
           <p>{ this.state.text }</p>
         </div>
+
+        <div
+          className="draggable edge"
+          style={{ top: -5, width: '100%', cursor: 'ns-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'top')}
+        />
+        <div
+          className="draggable edge"
+          style={{ bottom: -5, width: '100%', cursor: 'ns-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'bottom')}
+        />
+        <div
+          className="draggable edge"
+          style={{ right: -5, height: '100%', cursor: 'ew-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'right')}
+        />
+        <div
+          className="draggable edge"
+          style={{ left: -5, height: '100%', cursor: 'ew-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'left')}
+        />
+
+        <div
+          className="draggable corner"
+          style={{ top: -5, left: -5, cursor: 'nwse-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'topleft')}
+        />
+        <div
+          className="draggable corner"
+          style={{ top: -5, right: -5, cursor: 'nesw-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'topright')}
+        />
+        <div
+          className="draggable corner"
+          style={{ bottom: -5, left: -5, cursor: 'nesw-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'bottomleft')}
+        />
+        <div
+          className="draggable corner"
+          style={{ bottom: -5, right: -5, cursor: 'nwse-resize' }}
+          onMouseDown={(e) => this.handleMouseDown(e, 'bottomright')}
+        />
+
         <style jsx>{`
           .CardEditable {
+            position: relative;
             box-shadow: 1px 1px 3px #666;
             border-radius: 5px;
             background-color: #fafafa;
-            transition: all 80ms ease-in-out;
             align-items: center;
+            user-select: none;
           }
           .CardEditable:hover {
 
@@ -55,6 +163,14 @@ export default class CardEditable extends Component {
           .CardEditable p {
             font-size: 14px;
             text-align: left;
+          }
+          .CardEditable .draggable {
+            position: absolute;
+            width: 10px;
+            height: 10px;
+          }
+          .CardEditable .draggable:hover {
+            background-color: rgba(255,255,0,0.1);
           }
         `}
         </style>
